d153d1a9cea166df7852e8ca2077b79602baa8c4
KamrulHasan <kamrulhasan3288@gmail.com>
2022-11-12 22:16:09 +0600

    implement repo list vieModel

--- features/repolist/src/main/java/com/iamkamrul/repolist/RepoListViewModel.kt
+++ features/repolist/src/main/java/com/iamkamrul/repolist/RepoListViewModel.kt
@@ -1,9 +1,52 @@
 package com.iamkamrul.repolist
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.iamkamrul.domain.usecase.RepoListUseCase
+import com.iamkamrul.domain.utils.Result
+import com.iamkamrul.entity.RepoItemEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RepoListViewModel @Inject constructor(): ViewModel(){
+class RepoListViewModel @Inject constructor(
+    private val repoListUseCase: RepoListUseCase
+): ViewModel(){
+    val action:(RepoListUiAction)->Unit
+    private val _uiState = MutableStateFlow<RepoListUiState<*>>(RepoListUiState.Loading(false))
+    val uiState get() = _uiState
+
+    init {
+        action = {
+            when(it){
+                RepoListUiAction.FetchRepoList -> fetchRepoList()
+            }
+        }
+        fetchRepoList()
+    }
+
+    private fun fetchRepoList(){
+        viewModelScope.launch {
+            repoListUseCase.execute(RepoListUseCase.Params(userName = "kamrul322")).collect{result->
+                when(result){
+                    is Result.Error -> _uiState.value = RepoListUiState.Error(result.message)
+                    is Result.Loading -> _uiState.value = RepoListUiState.Loading(result.loading)
+                    is Result.Success -> _uiState.value = RepoListUiState.Success(result.data)
+                }
+            }
+        }
+    }
+
+}
+
+sealed class RepoListUiState<out R>{
+    data class Loading(val isLoading:Boolean):RepoListUiState<Loading>()
+    data class Success(val repoList:List<RepoItemEntity>):RepoListUiState<Success>()
+    data class Error(val message:String):RepoListUiState<Error>()
+}
+
+sealed class RepoListUiAction{
+    object FetchRepoList:RepoListUiAction()
 }
