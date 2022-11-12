00b9d747a9d17f46ddc8444f4fe0f235333360d9
KamrulHasan <kamrulhasan3288@gmail.com>
2022-11-12 20:03:55 +0600

    add repo module

---
+++ app/src/main/java/com/iamkamrul/modularization/di/RepositoryModule.kt
@@ -0,0 +1,9 @@
+package com.iamkamrul.modularization.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RepositoryModule
