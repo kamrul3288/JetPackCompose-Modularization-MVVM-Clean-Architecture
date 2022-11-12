ea682620f7678ca2326e9b9c8252f4dea26d7885
KamrulHasan <kamrulhasan3288@gmail.com>
2022-11-12 20:02:23 +0600

    add debug and release module

---
+++ app/src/debug/java/com/iamkamrul/modularization/BaseUrlModule.kt
@@ -0,0 +1,16 @@
+package com.iamkamrul.modularization
+
+import com.iamkamrul.di.qualifier.AppBaseUrl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class BaseUrlModule{
+    @Provides
+    @AppBaseUrl
+    fun provideBaseUrl():String = ""
+}
+
